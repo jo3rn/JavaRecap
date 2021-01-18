e1e1e571b5b48ce261818e9d15937b9f9cfc0f59
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-01-18 17:25:14 +0100

    Add cowsay (HA00-A0)

---
+++ 00-hallo_welt/Cowsay.java
@@ -0,0 +1,13 @@
+/* Hausaufgabe 00 Aufgabe 0 */
+public class Cowsay {
+  public static void main(String[] args) {
+    System.out.println("  ____________________");
+    System.out.println("< Ich gehöre zu jo3rn >");
+    System.out.println("  --------------------");
+    System.out.println("        \\   ^__^");
+    System.out.println("         \\  (oo)\\_______");
+    System.out.println("            (__)\\       )\\/\\");
+    System.out.println("                ||----w |");
+    System.out.println("                ||     ||");
+  }
+}
