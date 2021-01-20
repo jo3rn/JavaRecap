fbb38e89278f8232ef4a4c23ba77e6aa17a477ab
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-01-20 18:04:31 +0100

    Add UEB05-A4

--- 05-referenzdatentypen/StringFun.java
+++ 05-referenzdatentypen/StringFun.java
@@ -1,5 +1,5 @@
 /* Übung 05 Aufgabe 4
- * Link:
+ * Link: https://www.youtube.com/watch?v=Ps2tFlvyL3o
  */
 
 /* Studieren Sie die Dokumentation der String Klasse in Java und
@@ -8,6 +8,16 @@
 
 public class StringFun {
   public static void main(String[] args) {
+    String book = "book";
+    System.out.println(book.length()); // die Länge von 'book' ist 4
+    System.out.println(book.isEmpty()); // der String 'book' ist nicht leer
 
+    String look = book.replace('b', 'l'); // 'b' wird durch 'l' ersetzt
+    System.out.println(look); // look
+
+    System.out.println(book.compareTo(look)); // -10: 'b' steht vor 'l'
+
+    String upperLook = "Look";
+    System.out.println(book.compareTo(upperLook)); // 22: 'L' steht vor 'b'
   }
 }
