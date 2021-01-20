47371f4f7027fb3ce7b8ad377fa8564e36b15f51
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-01-20 11:10:07 +0100

    Add scaffolding for UEB04-A2 and HA04-A1

---
+++ 04-methoden/LuckySport.java
@@ -0,0 +1,35 @@
+/* Übung 04 Aufgabe 2
+ * Link:
+ */
+
+/* Betrachten Sie folgende Notentabellen im Leistungskurs Sport für Schülerinnen (links) und Schüler (rechts).
+ *
+ *  ----- Schülerinnen -----    -------- Schüler -------
+ *  Note  100m  200m  400m      Note  100m  200m  400m
+ *  1+    13.4  28.4  1:06.0    1+    12.0  24.7  56.7
+ *  1     13.6  28.6  1:07.0    1     12.1  25.0  57.2
+ *  ...
+ *  4+    15.6  32.2  1:19.0    4+    13.6  28.1  1:03.8
+ *  4     15.9  32.7  1:21.0    4     13.9  28.6  1:05.0
+ *  4-    16.2  33.2  1:24.0    4-    14.2  29.1  1:06.2
+ *  ...
+ *
+ * Die Methode public static double luckyPass(boolean isFemale, int distance) soll die Mindestzeit in Sekunden
+ * zurückliefern, die gebraucht wird um eine glatte 4 als Bewertung zu erreichen (z.B. 32.7 für Schülerinnen über 200m).
+ * Der formale Parameter isFemale wird als true übergeben für Schülerinnen, als false für Schüler.
+ * Der Parameter distance soll nur die Werte 100, 200 oder 400 annehmen.
+ * Wird ein anderer Wert für distance übergeben, soll die Methode -1 zurückgeben.
+ *
+ * Schreiben Sie ein Programm, welches die Methode luckyPass enthält und aufruft.
+ */
+
+public class LuckySport {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static double luckyPass(boolean isFemale, int distance) {
+
+  }
+}
---
+++ 04-methoden/Tombola.java
@@ -0,0 +1,30 @@
+/* Hausaufgabe 04 Aufgabe 1
+ * Link:
+ */
+
+/* Auf einem Tombola-Los stehen drei Ganzzahlen a, b und c.
+ * Der Gewinn beträgt 10 EUR, wenn alle drei Zahlen gleich 3 sind.
+ * Wenn nicht, gewinnt man 5 EUR, falls alle drei Zahlen gleich sind.
+ * Falls das auch nicht der Fall ist, gewinnt man zumindest 1 EUR, wenn alle drei Zahlen unterschiedlich sind.
+ * In allen anderen Fällen beträgt der Gewinn 0 EUR.
+ * Schreiben Sie die Methode public static int tombola(int a, int b, int c), welche den Gewinn
+ * für eine gegebene Ziffernfolge eines Loses zurückliefert.
+ *
+ * Beispiele:
+ * tombola(3, 3, 3) -> 10
+ * tombola(9, 9, 9) -> 5
+ * tombola(1, 2, 3) -> 1
+ * tombola(1, 2, 2) -> 0
+ *
+ */
+
+public class Tombola {
+
+  public static void main(String[] args) {
+
+  }
+
+  public static int tombola(int a, int b, int c) {
+
+  }
+}
