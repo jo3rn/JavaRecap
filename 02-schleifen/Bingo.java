c8f759e1dd3edb00f6057b3bfc1b200f1705a3bb
Jörn <12082310+jo3rn@users.noreply.github.com>
2021-01-20 19:26:18 +0100

    Add UEB02-A4 & HA02-A4
    * Add bingo (UEB02-A4)
    
    * Add dices (HA02-A4)
    
    * Fix line length and variable naming (UEB02-A4, HA02-A4)

---
+++ 02-schleifen/Bingo.java
@@ -0,0 +1,66 @@
+/* Übung 02 Aufgabe 4
+ * Link: https://www.youtube.com/watch?v=mgdoiBYMXXQ
+ */
+
+/* Schreiben Sie ein Programm, welches ein simples Bingo-Spiel simuliert (für nur eine Spieler*in).
+ * Denken Sie sich 4 verschiedene Ganzzahlen zwischen 0 und 20 (einschließlich) aus
+ * und legen Sie diese Zahlen als Variablen an.
+ * Lassen Sie die Spieler*in so lange Zahlen eingeben, bis sie alle vier Bingo-Zahlen ‚erraten‘ hat
+ * und geben Sie dann "Bingo!" auf der Konsole aus. Das Programm endet dann.
+ * Außerdem soll das Programm enden, wenn die Spieler*in eine negative Zahl eintippt
+ * (also wohl keine Lust mehr hat).
+
+ * Erweiterung für zwei Spieler*innen, falls Sie Spaß daran haben:
+ * Lassen Sie zu Beginn des Spiels eine Spieler*in die vier Zahlen selbst eingeben über die Konsole.
+ * Dann geben Sie so viele Leerzeilen aus, dass eine zweite Spieler*in die Zahlen nicht mehr sieht.
+ * Nun kann die zweite Spieler*in so lange raten, bis sie ein Bingo! hat (oder aufgibt).
+ * Geben Sie die Anzahl an Versuchen beim Bingo!
+ */
+
+import java.util.Scanner;
+
+public class Bingo {
+  public static void main(String[] args) {
+    boolean isAGuessed = false;
+    boolean isBGuessed = false;
+    boolean isCGuessed = false;
+    boolean isDGuessed = false;
+
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Bitte Zahl Nr. 1 eingeben:");
+    int a = in.nextInt();
+    System.out.println("Bitte Zahl Nr. 2 eingeben:");
+    int b = in.nextInt();
+    System.out.println("Bitte Zahl Nr. 3 eingeben:");
+    int c = in.nextInt();
+    System.out.println("Bitte Zahl Nr. 4 eingeben:");
+    int d = in.nextInt();
+
+    for (int i = 0; i < 100; i++) {
+      System.out.println();
+    }
+
+    int guess = 0;
+    while ((!isAGuessed || !isBGuessed || !isCGuessed || !isDGuessed) && guess >= 0) {
+      System.out.println("Bitte nächste Zahl eingeben: ");
+      guess = in.nextInt();
+
+      if (guess == a) {
+        isAGuessed = true;
+      } else if (guess == b) {
+        isBGuessed = true;
+      } else if (guess == c) {
+        isCGuessed = true;
+      } else if (guess == d) {
+        isDGuessed = true;
+      }
+    }
+
+    if (guess >= 0) {
+      System.out.println("Bingo!");
+    } else {
+      System.out.println("Das Spiel wurde frühzeitig beendet.");
+    }
+  }
+}
---
+++ 02-schleifen/SumOfThreeDice.java
@@ -0,0 +1,55 @@
+/* Hausaufgabe 02 Aufgabe 4
+ * Link: https://www.youtube.com/watch?v=icpePOGkyCs
+ */
+
+/* Schreiben Sie ein Programm SumOfThreeDice,
+ * das alle folgenden Kombinationen für das Würfeln mit drei Würfeln ausgibt:
+ * Die Summe der Würfelaugen muss mindestens 7 betragen und
+ * für (a, b, c) muss gelten, dass a <= b <= c.
+ * Zum Beispiel soll (2,2,3) ausgegeben werden, aber (3,2,2) ebenso wenig wie (1,2,3).
+ */
+
+public class SumOfThreeDice {
+  public static void main(String[] args) {
+
+    int count = 0;
+
+    /* straight forward */
+    for (int a = 1; a < 7; a++) {
+      count++;
+
+      for (int b = 1; b < 7; b++) {
+        count++;
+
+        for (int c = 1; c < 7; c++) {
+          count++;
+          if (a + b + c > 6 && a <= b && b <= c) {
+            System.out.println("(" + a + ", " + b + ", " + c + ")");
+          }
+        }
+      }
+    }
+
+    System.out.println(count);
+
+    count = 0;
+
+    /* optimized */
+    for (int a = 1; a < 7; a++) {
+      count++;
+
+      for (int b = a; b < 7; b++) {
+        count++;
+
+        for (int c = b; c < 7; c++) {
+          count++;
+          if (a + b + c > 6) {
+            System.out.println("(" + a + ", " + b + ", " + c + ")");
+          }
+        }
+      }
+    }
+
+    System.out.println(count);
+  }
+}
