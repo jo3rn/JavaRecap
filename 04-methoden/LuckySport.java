b19ecb3f98f95d6e0b48faac9f77c2f35c86133b
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-01-20 13:45:14 +0100

    Fix line length (UEB04-A2, HA04-A1)

--- 04-methoden/LuckySport.java
+++ 04-methoden/LuckySport.java
@@ -2,7 +2,8 @@
  * Link:
  */
 
-/* Betrachten Sie folgende Notentabellen im Leistungskurs Sport für Schülerinnen (links) und Schüler (rechts).
+/* Betrachten Sie folgende Notentabellen im Leistungskurs Sport für Schülerinnen (links)
+ * und Schüler (rechts).
  *
  *  ----- Schülerinnen -----    -------- Schüler -------
  *  Note  100m  200m  400m      Note  100m  200m  400m
@@ -14,8 +15,9 @@
  *  4-    16.2  33.2  1:24.0    4-    14.2  29.1  1:06.2
  *  ...
  *
- * Die Methode public static double luckyPass(boolean isFemale, int distance) soll die Mindestzeit in Sekunden
- * zurückliefern, die gebraucht wird um eine glatte 4 als Bewertung zu erreichen (z.B. 32.7 für Schülerinnen über 200m).
+ * Die Methode public static double luckyPass(boolean isFemale, int distance) soll die Mindestzeit
+ * in Sekunden zurückliefern, die gebraucht wird um eine glatte 4 als Bewertung zu erreichen
+ * (z.B. 32.7 für Schülerinnen über 200m).
  * Der formale Parameter isFemale wird als true übergeben für Schülerinnen, als false für Schüler.
  * Der Parameter distance soll nur die Werte 100, 200 oder 400 annehmen.
  * Wird ein anderer Wert für distance übergeben, soll die Methode -1 zurückgeben.
@@ -26,7 +28,6 @@
 public class LuckySport {
 
   public static void main(String[] args) {
-
   }
 
   public static double luckyPass(boolean isFemale, int distance) {
--- 04-methoden/Tombola.java
+++ 04-methoden/Tombola.java
@@ -5,10 +5,11 @@
 /* Auf einem Tombola-Los stehen drei Ganzzahlen a, b und c.
  * Der Gewinn beträgt 10 EUR, wenn alle drei Zahlen gleich 3 sind.
  * Wenn nicht, gewinnt man 5 EUR, falls alle drei Zahlen gleich sind.
- * Falls das auch nicht der Fall ist, gewinnt man zumindest 1 EUR, wenn alle drei Zahlen unterschiedlich sind.
+ * Falls das auch nicht der Fall ist, gewinnt man zumindest 1 EUR,
+ * wenn alle drei Zahlen unterschiedlich sind.
  * In allen anderen Fällen beträgt der Gewinn 0 EUR.
- * Schreiben Sie die Methode public static int tombola(int a, int b, int c), welche den Gewinn
- * für eine gegebene Ziffernfolge eines Loses zurückliefert.
+ * Schreiben Sie die Methode public static int tombola(int a, int b, int c),
+ * welche den Gewinn für eine gegebene Ziffernfolge eines Loses zurückliefert.
  *
  * Beispiele:
  * tombola(3, 3, 3) -> 10
