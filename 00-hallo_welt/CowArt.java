b182debd5c94ba7a75952032f36bc3f4f0c1db58
Jörn <12082310+jo3rn@users.noreply.github.com>
2021-01-20 19:25:23 +0100

    Add HA00-A0
    * Add cowsay (HA00-A0)
    
    * Add description (HA00-A0)
    
    * Add link (HA00-A0)
    
    * Fix wording (HA00-A0)
    
    * Fix line length (HA00-A0)

---
+++ 00-hallo_welt/CowArt.java
@@ -0,0 +1,29 @@
+/* Hausaufgabe 00 Aufgabe 0
+ * Link: https://www.youtube.com/watch?v=7ob7s8VVSzM
+ */
+
+/* Programmieren Sie nur mit den Kenntnissen der SU 0 eine Klasse CowArt, die beim Ausführen acht
+ * Zeilen Text auf der Konsole ausgibt, und zwar derart, dass ein Bild entsteht
+ * (das nennt man ASCII-Art). Das Bild soll wie folgt aussehen:
+     ________________________
+   < Ich gehöre zu Niemandem >
+     ------------------------
+           \   ^__^
+            \  (oo)\_______
+               (__)\       )\/\
+                   ||----w |
+                   ||     ||
+
+ * Anstelle des Wortes Niemandem soll Ihr Gruppenname ausgegeben werden. */
+public class CowArt {
+  public static void main(String[] args) {
+    System.out.println("  ____________________");
+    System.out.println("< Ich gehöre zu jo3rn >");
+    System.out.println("  --------------------");
+    System.out.println("        \\   ^__^");
+    System.out.println("         \\  (oo)\\_______");
+    System.out.println("            (__)\\       )\\/\\");
+    System.out.println("                ||----w |");
+    System.out.println("                ||     ||");
+  }
+}
