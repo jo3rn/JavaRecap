efa4516e303f726b243d713fb8677f700e0ca561
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-01-20 11:58:26 +0100

    Fix wording (HA00-A0)

--- 00-hallo_welt/CowArt.java
+++ 00-hallo_welt/CowArt.java
@@ -3,7 +3,7 @@
  */
 
 /* Programmieren Sie nur mit den Kenntnissen der SU 0 eine Klasse CowArt, die beim Ausführen acht Zeilen Text auf der
- * Konsole ausgibt, und zwar derart, dass ein Bild entsteht (das nennt man ACII-Art). Das Bild soll wie folgt aussehen:
+ * Konsole ausgibt, und zwar derart, dass ein Bild entsteht (das nennt man ASCII-Art). Das Bild soll wie folgt aussehen:
      ________________________
    < Ich gehöre zu Niemandem >
      ------------------------
@@ -14,7 +14,7 @@
                    ||     ||
 
  * Anstelle des Wortes Niemandem soll Ihr Gruppenname ausgegeben werden. */
-public class Cowsay {
+public class CowArt {
   public static void main(String[] args) {
     System.out.println("  ____________________");
     System.out.println("< Ich gehöre zu jo3rn >");
