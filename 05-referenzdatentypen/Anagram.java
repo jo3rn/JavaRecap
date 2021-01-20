557472d4f7e15e2e12ae27abe5996218053c1856
Joern Auerbach <joern.auerbach@informatik.hs-fulda.de>
2021-01-20 19:18:08 +0100

    Add HA05-A3

--- 05-referenzdatentypen/Anagram.java
+++ 05-referenzdatentypen/Anagram.java
@@ -1,5 +1,5 @@
 /* Hausaufgabe 05 Aufgabe 3
- * Link:
+ * Link: https://www.youtube.com/watch?v=4G25kaQvuwc
  */
 
 /* Ein Anagramm einer Zeichenkette ist eine Zeichenkette mit exakt gleich vielen,
@@ -8,7 +8,7 @@
  * Außerdem sollen Leerzeichen ignoriert werden, so dass auch
  * "tom marvolo riddle" und "i am lord voldemort" Anagramme voneinander sind.
  * Klein-/Großschreibung soll hingegen berücksichtigt werden.
- * Schreiben Sie eine Methode public static boolean anagramm(String s1, String s2), die prüft,
+ * Schreiben Sie eine Methode public static boolean anagram(String s1, String s2), die prüft,
  * ob die beiden Argumente Anagramme voneinander sind. Testen Sie in der main!
  *
  * Gehen Sie dabei wie folgt vor (und nicht anders, etwa Sortieren mit eingebauten Methoden):
@@ -18,10 +18,34 @@
 
 public class Anagram {
   public static void main(String[] args) {
-
+    System.out.println("Testing anagram method... passes if all are true:");
+    System.out.println(anagram("anagram", "anagram") == true);
+    System.out.println(anagram("lagerregal", "regallager") == true);
+    System.out.println(anagram("programmieren", "oper immer rang") == true);
+    System.out.println(anagram("tom marvolo riddle", "i am lord voldemort") == true);
+    System.out.println(anagram("java", "cpluplus") == false);
+    System.out.println(anagram("warmer regen", "kalte dusche") == false);
   }
 
   public static boolean anagram(String s1, String s2) {
+    s1 = s1.replaceAll(" ", "");
+    s2 = s2.replaceAll(" ", "");
 
+    if (s1.length() != s2.length()) {
+      return false;
+    }
+
+    for (int i = 0; i < s1.length(); i++) {
+      char c = s1.charAt(i);
+      int index = s2.indexOf(c);
+
+      if (index == -1) {
+        return false;
+      }
+
+      s2 = s2.substring(0, index) + s2.substring(index + 1);
+    }
+
+    return true;
   }
 }
